205be573485e734fcfa2348824772e639f504432
Gergo Szatmari <v-gszatmari@expedia.com>
2019-05-07 15:02:43 +0200

    rethinked the python script again

--- link_yml2json.py
+++ link_yml2json.py
@@ -2,24 +2,28 @@
 import yaml
 import json
 
-stream = open('links.yml', 'r')
+def main():
+    stream = open('links.yml', 'r')
 
-groups = yaml.safe_load(stream)
+    groups = yaml.safe_load(stream)
 
-f = open('links.json', 'w')
-
-data = []
-for links in groups:
-    group = {
-        "name": links.keys()[0],
-        "links" : []
-    }
-    for link in links[links.keys()[0]]:
-        item = {
-            "name": link["name"],
-            "url": link["url"]
+    data = []
+    for links in groups:
+        group = {
+            "name": links.keys()[0],
+            "links" : []
         }
-        group["links"].append(item)
-    data.append(group)
-f.write("data = " + json.dumps(data))
-f.close()
+        for link in links[links.keys()[0]]:
+            item = {
+                "name": link["name"],
+                "url": link["url"]
+            }
+            group["links"].append(item)
+        data.append(group)
+        
+    f = open('links.json', 'w')
+    f.write("data = " + json.dumps(data))
+    f.close()
+    
+if __name__ == "__main__":
+    main()
