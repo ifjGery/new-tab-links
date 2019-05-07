4a05bcfdc0c8983a89da7d33549d0d65af1575e9
Gergo Szatmari <v-gszatmari@expedia.com>
2019-05-07 15:43:12 +0200

    even more pythonic

--- link_yml2json.py
+++ link_yml2json.py
@@ -2,26 +2,26 @@
 import yaml
 import json
 
+def makeItem(links):
+    name = links.keys()[0]
+    return { 
+        "name": name, 
+        "links" : [ 
+            {
+                "name": link["name"], 
+                "url": link["url"] 
+            } for link in links[name]
+        ] 
+    }
+
 def main():
-    stream = open('links.yml', 'r')
+    with open('links.yml', 'r') as stream:
+        groups = yaml.safe_load(stream)
 
-    groups = yaml.safe_load(stream)
-
-    data = [ 
-        { 
-            "name": links.keys()[0], 
-            "links" : [ 
-                {
-                    "name": link["name"], 
-                    "url": link["url"] 
-                } for link in links[links.keys()[0]] 
-            ] 
-        } for links in groups 
-    ]
+        data = [ makeItem(links) for links in groups ]
         
-    f = open('links.json', 'w')
-    f.write("data = " + json.dumps(data))
-    f.close()
+        with open('links.json', 'w') as file:
+            file.write("data = " + json.dumps(data))
     
 if __name__ == "__main__":
     main()
